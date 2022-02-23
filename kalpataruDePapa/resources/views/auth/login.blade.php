65db74622db46c22c20be9f6e7043e7494c61d7a
feersuarez <76107506+feersuarez@users.noreply.github.com>
2022-02-23 13:44:32 +0100

    ewstilos login y registro

--- kalpataruDePapa/resources/views/auth/login.blade.php
+++ kalpataruDePapa/resources/views/auth/login.blade.php
@@ -7,18 +7,18 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header"><h1 class="titulos">{!! trans('jokes.InicioSesion') !!}</h1></div>
+            <div class="card" style="background-color: rgba(0,0,0,0.2); margin-top: 10%;">
+                <div class="card-header" style=""><h1 class="titulos">{!! trans('jokes.InicioSesion') !!}</h1></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.CorreoInicio') !!}</label>
+                            <label for="email" id="correoInicio" style="color: #b9ffff;" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.CorreoInicio') !!}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" style="background-color: rgba(255, 255, 255, 0.5); margin-top: 1%; border: none; font-family: 'Helvetica';" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -29,10 +29,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.ContraSesion') !!}</label>
+                            <label for="password" style="color: #b9ffff; margin-top: 1%;" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.ContraSesion') !!}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" style="background-color: rgba(255, 255, 255, 0.5); margin-top: 1%; border: none;" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -47,7 +47,7 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
+                                    <label class="form-check-label" style="color: #b9ffff;" for="remember">
                                         {!! trans('jokes.RecordarSesion') !!}
                                     </label>
                                 </div>
@@ -56,12 +56,12 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" style="background-color: #D401D6; border: none; color: #b9ffff;">
                                     {!! trans('jokes.IniciarSesion') !!}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link" style="color: #D401D6;" href="{{ route('password.request') }}">
                                         {!! trans('jokes.OlvidasteSesion') !!}
                                     </a>
                                 @endif
--- kalpataruDePapa/resources/views/auth/register.blade.php
+++ kalpataruDePapa/resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@ $curso= App\Models\Curso::all();
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card" style="background-color: rgba(0,0,0,0.2); margin-top: 10%;">
                 <div class="card-header"><h1 class="titulos">{!! trans('jokes.Registrarse') !!}</h1></div>
 
                 <div class="card-body">
@@ -15,10 +15,10 @@ $curso= App\Models\Curso::all();
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.NombreRegistro') !!}</label>
+                            <label for="name" style="color: #b9ffff; margin-top: 1%;" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.NombreRegistro') !!}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" style="background-color: rgba(255, 255, 255, 0.5); margin-top: 1%; border: none; font-family: 'Helvetica';" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -29,9 +29,9 @@ $curso= App\Models\Curso::all();
                         </div>
 
                         <div class="form-group row">
-                            <label for="CodCurso" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.CursoRegistro') !!}</label>
+                            <label for="CodCurso" style="color: #b9ffff; margin-top: 1%;" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.CursoRegistro') !!}</label>
                             <div class="col-md-6">
-                            <select class="form-select" aria-label="Default select example" name="codCurso">
+                            <select class="form-select" style="background-color: rgba(255, 255, 255, 0.5); margin-top: 1%; border: none;" aria-label="Default select example" name="codCurso">
                                 <option selected>{!! trans('jokes.SelectCurso') !!}</option>
                                 @foreach($curso as $curso)
                                 <option>{{$curso->nombre}}</option>
@@ -42,10 +42,10 @@ $curso= App\Models\Curso::all();
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.CorreoRegistro') !!}</label>
+                            <label for="email" style="color: #b9ffff; margin-top: 1%;" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.CorreoRegistro') !!}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" style="background-color: rgba(255, 255, 255, 0.5); margin-top: 1%; border: none; font-family: 'Helvetica';" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -56,10 +56,10 @@ $curso= App\Models\Curso::all();
                         </div>
 
                             <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.ContraRegistro') !!}</label>
+                            <label for="password" style="color: #b9ffff; margin-top: 1%;" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.ContraRegistro') !!}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" style="background-color: rgba(255, 255, 255, 0.5); margin-top: 1%; border: none;" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -71,17 +71,17 @@ $curso= App\Models\Curso::all();
                                
                             
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.RepetirRegistro') !!}</label>
+                            <label for="password-confirm" style="color: #b9ffff; margin-top: 1%;" class="col-md-4 col-form-label text-md-right">{!! trans('jokes.RepetirRegistro') !!}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" style="background-color: rgba(255, 255, 255, 0.5); margin-top: 1%; border: none;" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
          
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" style="background-color: #D401D6; border: none; color: #b9ffff; margin-top: 1%;">
                                     {!! trans('jokes.RegistroBtn') !!}
                                 </button>
                             </div>
